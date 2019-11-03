6c1ee498c5e05d0d2b54778a9d4fde523b9cc0ed
Andrivsky <52834744+Andrivsky@users.noreply.github.com>
2019-11-03 15:31:55 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -26,6 +26,7 @@ def send_message(chat_id, text='Wait'):
     url = URL + 'sendmessage?chat_id={}&text={}'.format(chat_id,text)
     requests.get(url)
 
+    
 def main():
     answer = get_message()
     chat_id = answer['chat_id']
@@ -44,30 +45,7 @@ def main():
         send_message(chat_id,result(link=a,region='2'))
     else:
         send_message(chat_id, 'Чтобы начать работу бота введите команду "\pars"')
-    # with open('updates.json','w')as file:
-    #     json.dump(d, file, indent=2, ensure_ascii=False)
+
 
 if __name__ == '__main__':
     main()
-
-
-#https://api.telegram.org/bot976945954:AAFifF-U7bqBCkcHnDpuy-hK4I4zuO_2VH0/sendmessage?chat_id=460967713&text=hi
-#https://core.telegram.org/bots/api#making-requests
-#https://www.youtube.com/watch?v=iMBuy0INnHQ&list=PLlWXhlUMyooaTZA4vxU9ZRZQPCFxUq9VA&index=4 26 min
-
-    # if text == '\pars':
-    #     send_message(chat_id, 'Отправь ссыль')
-    #     time.sleep(5)
-    #     answer = get_message()
-    #     text = answer['text']
-    #     link = str(text)
-    #     send_message(chat_id, 'Какой регион?')
-    #     time.sleep(5)
-    #     answer = get_message()
-    #     text = str(answer['text'])
-    #     time.sleep(5)
-    #     if text == '2':
-    #         time.sleep(5)
-    #         send_message(chat_id, misc.result(link=link,region='2'))
-    #     # send_message(chat_id,misc.result('2','2'))
-
